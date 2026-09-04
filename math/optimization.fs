fc4c454dfd521e0819b5fc9f2171abf8dfd78022
Sugisaka <sugisaka@mail.kitami-it.ac.jp>
2026-09-04 12:34:37 +0900

    [UPDATE] optimization

--- math/optimization.fs
+++ math/optimization.fs
@@ -6,6 +6,10 @@
 //
 namespace Aqualis
 
+module private OptimizationDefaults =
+    [<Literal>]
+    let MaxBracketExpansions = 64
+
 type ContextOptimization internal (context:Aqualis) =
 
     /// <summary>
@@ -17,7 +21,11 @@ type ContextOptimization internal (context:Aqualis) =
     /// <param name="dd">探索幅</param>
     /// <param name="f">目的関数</param>
     /// <param name="xx">fが極小となるベクトル</param>
-    member this.findmin (m:int) (x0_:double1,df:double1) (dd:double0) (f:double0->double1->unit) (xx:double1) =
+    member private this.findminCore (m:int) (maxBracketExpansions:int) (x0_:double1,df:double1) (dd:double0) (f:double0->double1->unit) (xx:double1) =
+        if m < 0 then
+            invalidArg (nameof m) "The line-search iteration count cannot be negative."
+        if maxBracketExpansions < 0 then
+            invalidArg (nameof maxBracketExpansions) "The bracket-expansion limit cannot be negative."
         let r = 0.5*(1.0+sqrt(5.0))
         context.ch.d1 x0_.size1 <| fun xa ->
         context.ch.d1 x0_.size1 <| fun x1 ->
@@ -26,7 +34,7 @@ type ContextOptimization internal (context:Aqualis) =
         context.ch.dddd <| fun (fa,f1,f2,fb) ->
         context.ch.d <| fun fa_ ->
         context.ch.d <| fun norm_df ->
-        context.ch.i <| fun counter ->
+        context.ch.iii <| fun (counter,expansionCounter,expansionLimitReached) ->
             norm_df.clear()
             context.iter.num df.size1 <| fun i ->
                 norm_df <== norm_df + df.[i]*df.[i]
@@ -40,19 +48,25 @@ type ContextOptimization internal (context:Aqualis) =
             x2.foreach <| fun i -> x2.[i] <== xa.[i] + (xb.[i]-xa.[i])/r
             f f2 x2
             counter.clear()
+            expansionCounter.clear()
+            expansionLimitReached.clear()
             fa_ <== fa
-            context.iter.whiledo (counter.<m) <| fun _ ->
+            context.iter.whiledo (And [counter.<m; expansionLimitReached.=0]) <| fun _ ->
                 context.br.branch <| fun b ->
                     b.IF (fa .> f1 .> f2 .> fb) <| fun () ->
-                        counter.dec()
-                        //xa: そのまま
-                        x1 <== x2
-                        f1 <== f2
-                        x2 <== xb
-                        f2 <== fb
-                        //xb: 新規計算
-                        xb.foreach <| fun i -> xb.[i] <== xa.[i] + (x1.[i]-xa.[i])*(1.0+r)
-                        f fb xb
+                        context.br.if2 (expansionCounter.<maxBracketExpansions)
+                        <| fun () ->
+                            expansionCounter.inc()
+                            //xa: そのまま
+                            x1 <== x2
+                            f1 <== f2
+                            x2 <== xb
+                            f2 <== fb
+                            //xb: 新規計算
+                            xb.foreach <| fun i -> xb.[i] <== xa.[i] + (x1.[i]-xa.[i])*(1.0+r)
+                            f fb xb
+                        <| fun () ->
+                            expansionLimitReached <== 1
                     b.IF (And [f1.>f2; fa.>f2;]) <| fun () ->
                         counter.inc()
                         xa <== x1
@@ -75,7 +89,20 @@ type ContextOptimization internal (context:Aqualis) =
                         f f1 x1
                     b.EL <| fun () ->
                         context.print.s "error: findmin"
-            xx.foreach <| fun i -> xx.[i] <== 0.5*(xa.[i]+xb.[i])
+            context.br.if2 (expansionLimitReached.=0)
+            <| fun () ->
+                xx.foreach <| fun i -> xx.[i] <== 0.5*(xa.[i]+xb.[i])
+            <| fun () ->
+                xx <== xb
+                context.print.s "Aqualis: line-search bracket expansion limit reached."
+
+    /// 探索区間の拡張回数に既定の上限を用いて、直線上の極小値を探索します。
+    member this.findmin (m:int) (x0_:double1,df:double1) (dd:double0) (f:double0->double1->unit) (xx:double1) =
+        this.findminCore m OptimizationDefaults.MaxBracketExpansions (x0_,df) dd f xx
+
+    /// 探索区間の拡張回数に上限を指定して、直線上の極小値を探索します。
+    member this.findminWithBracketLimit (m:int,maxBracketExpansions:int) (x0_:double1,df:double1) (dd:double0) (f:double0->double1->unit) (xx:double1) =
+        this.findminCore m maxBracketExpansions (x0_,df) dd f xx
 
     /// <summary>
     /// 最急降下法
@@ -87,7 +114,7 @@ type ContextOptimization internal (context:Aqualis) =
     /// <param name="f">目的関数</param>
     /// <param name="df">目的関数の勾配</param>
     /// <param name="stepProc">各ステップの暫定解に対して行う処理</param>
-    member this.findmin_GradientDescent (n:int,m:int) (x0:double1) (dd0:double0) (f:double0->double1->unit) (df:double1->double1->unit) (stepProc:((int0*double1)->unit)option) =
+    member private this.findminGradientDescentCore (n:int,m:int,maxBracketExpansions:int) (x0:double1) (dd0:double0) (f:double0->double1->unit) (df:double1->double1->unit) (stepProc:((int0*double1)->unit)option) =
         match stepProc with
           |Some(pu) ->
                 pu (_0,x0)
@@ -106,12 +133,20 @@ type ContextOptimization internal (context:Aqualis) =
                             context.br.if1 (nr.=0.0) <| fun () ->
                                 ext()
                         //勾配方向に最小値を探す
-                        this.findmin m (x0,y) dd0_ f x0
+                        this.findminWithBracketLimit (m,maxBracketExpansions) (x0,y) dd0_ f x0
                     match stepProc with
                       |Some(pu) ->
                             pu(i,x0)
                       |None ->
                         ()
+
+    /// 既定の探索区間拡張上限を用いて、最急降下法で極小値を探索します。
+    member this.findmin_GradientDescent (n:int,m:int) (x0:double1) (dd0:double0) (f:double0->double1->unit) (df:double1->double1->unit) (stepProc:((int0*double1)->unit)option) =
+        this.findminGradientDescentCore (n,m,OptimizationDefaults.MaxBracketExpansions) x0 dd0 f df stepProc
+
+    /// 探索区間の拡張回数に上限を指定して、最急降下法で極小値を探索します。
+    member this.findmin_GradientDescentWithBracketLimit (n:int,m:int,maxBracketExpansions:int) (x0:double1) (dd0:double0) (f:double0->double1->unit) (df:double1->double1->unit) (stepProc:((int0*double1)->unit)option) =
+        this.findminGradientDescentCore (n,m,maxBracketExpansions) x0 dd0 f df stepProc
 
     /// <summary>
     /// ヘッセ行列を用いた共役勾配法
@@ -123,7 +158,7 @@ type ContextOptimization internal (context:Aqualis) =
     /// <param name="f">目的関数</param>
     /// <param name="df">目的関数の勾配</param>
     /// <param name="fH">目的関数のヘッセ行列</param>
-    member this.findmin_ConjugateGradient1 (n:int,m:int) (x0:double1) (dd0:double0) (f:double0->double1->unit) (df:double1->double1->unit) (fH:double2->double1->unit) (stepProc:((int0*double1)->unit)option) =
+    member private this.findminConjugateGradient1Core (n:int,m:int,maxBracketExpansions:int) (x0:double1) (dd0:double0) (f:double0->double1->unit) (df:double1->double1->unit) (fH:double2->double1->unit) (stepProc:((int0*double1)->unit)option) =
         match stepProc with
           |Some(pu) ->
                 pu (_0,x0)
@@ -156,7 +191,7 @@ type ContextOptimization internal (context:Aqualis) =
                         context.la.norm y <| fun nr ->
                             context.br.if1 (nr.=0.0) <| fun () -> ext()
                         //勾配方向に最小値を探す
-                        this.findmin m (x0,y) dd0_ f x0
+                        this.findminWithBracketLimit (m,maxBracketExpansions) (x0,y) dd0_ f x0
                         match stepProc with
                           |Some pu ->
                                 pu(i,x0)
@@ -164,6 +199,14 @@ type ContextOptimization internal (context:Aqualis) =
                             ()
                         y.foreach <| fun i ->
                             b.[i] <== y.[i]
+
+    /// 既定の探索区間拡張上限を用いて、共役勾配法で極小値を探索します。
+    member this.findmin_ConjugateGradient1 (n:int,m:int) (x0:double1) (dd0:double0) (f:double0->double1->unit) (df:double1->double1->unit) (fH:double2->double1->unit) (stepProc:((int0*double1)->unit)option) =
+        this.findminConjugateGradient1Core (n,m,OptimizationDefaults.MaxBracketExpansions) x0 dd0 f df fH stepProc
+
+    /// 探索区間の拡張回数に上限を指定して、共役勾配法で極小値を探索します。
+    member this.findmin_ConjugateGradient1WithBracketLimit (n:int,m:int,maxBracketExpansions:int) (x0:double1) (dd0:double0) (f:double0->double1->unit) (df:double1->double1->unit) (fH:double2->double1->unit) (stepProc:((int0*double1)->unit)option) =
+        this.findminConjugateGradient1Core (n,m,maxBracketExpansions) x0 dd0 f df fH stepProc
 
     /// <summary>
     /// ヘッセ行列を用いない共役勾配法
@@ -174,7 +217,7 @@ type ContextOptimization internal (context:Aqualis) =
     /// <param name="dd0">探索間隔</param>
     /// <param name="f">目的関数</param>
     /// <param name="df">目的関数の勾配</param>
-    member this.findmin_ConjugateGradient2 (n:int,m:int) (x0:double1) (dd0:double0) (f:double0->double1->unit) (df:double1->double1->unit) (stepProc:((int0*double1)->unit)option) =
+    member private this.findminConjugateGradient2Core (n:int,m:int,maxBracketExpansions:int) (x0:double1) (dd0:double0) (f:double0->double1->unit) (df:double1->double1->unit) (stepProc:((int0*double1)->unit)option) =
         match stepProc with
           |Some pu ->
                 pu (_0,x0)
@@ -210,7 +253,7 @@ type ContextOptimization internal (context:Aqualis) =
                                 ext()
                         //勾配方向に最小値を探す
                         context.ch.d1 x0.size1 <| fun x ->
-                            this.findmin m (x0,y) dd0_ f x
+                            this.findminWithBracketLimit (m,maxBracketExpansions) (x0,y) dd0_ f x
                             x0 <== x
                             match stepProc with
                               |Some(pu) ->
@@ -222,6 +265,14 @@ type ContextOptimization internal (context:Aqualis) =
                         df1.foreach <| fun i ->
                             df1.[i] <== df0.[i]
 
+    /// 既定の探索区間拡張上限を用いて、共役勾配法で極小値を探索します。
+    member this.findmin_ConjugateGradient2 (n:int,m:int) (x0:double1) (dd0:double0) (f:double0->double1->unit) (df:double1->double1->unit) (stepProc:((int0*double1)->unit)option) =
+        this.findminConjugateGradient2Core (n,m,OptimizationDefaults.MaxBracketExpansions) x0 dd0 f df stepProc
+
+    /// 探索区間の拡張回数に上限を指定して、共役勾配法で極小値を探索します。
+    member this.findmin_ConjugateGradient2WithBracketLimit (n:int,m:int,maxBracketExpansions:int) (x0:double1) (dd0:double0) (f:double0->double1->unit) (df:double1->double1->unit) (stepProc:((int0*double1)->unit)option) =
+        this.findminConjugateGradient2Core (n,m,maxBracketExpansions) x0 dd0 f df stepProc
+
     /// <summary>
     /// ニュートン法
     /// </summary>
@@ -232,7 +283,7 @@ type ContextOptimization internal (context:Aqualis) =
     /// <param name="f"></param>
     /// <param name="df"></param>
     /// <param name="fH"></param>
-    member this.findmin_Newton (n:int,m:int) (x0:double1) (dd0:double0) (f:double0->double1->unit) (df:double1->double1->unit) (fH:double2->double1->unit) (stepProc:((int0*double1)->unit)option) =
+    member private this.findminNewtonCore (n:int,m:int,maxBracketExpansions:int) (x0:double1) (dd0:double0) (f:double0->double1->unit) (df:double1->double1->unit) (fH:double2->double1->unit) (stepProc:((int0*double1)->unit)option) =
         match stepProc with
           |Some pu ->
                 pu (_0,x0)
@@ -252,12 +303,20 @@ type ContextOptimization internal (context:Aqualis) =
                                 context.br.if1 (nr.=0.0) <| fun () -> ext()
                             a.foreach <| fun j ->
                                 a.[j] <== -a.[j]
-                            this.findmin m (x0,a) dd0_ f x0
+                            this.findminWithBracketLimit (m,maxBracketExpansions) (x0,a) dd0_ f x0
                             match stepProc with
                               |Some pu ->
                                     pu(i,x0)
                               |None ->
                                 ()
+
+    /// 既定の探索区間拡張上限を用いて、ニュートン法で極小値を探索します。
+    member this.findmin_Newton (n:int,m:int) (x0:double1) (dd0:double0) (f:double0->double1->unit) (df:double1->double1->unit) (fH:double2->double1->unit) (stepProc:((int0*double1)->unit)option) =
+        this.findminNewtonCore (n,m,OptimizationDefaults.MaxBracketExpansions) x0 dd0 f df fH stepProc
+
+    /// 探索区間の拡張回数に上限を指定して、ニュートン法で極小値を探索します。
+    member this.findmin_NewtonWithBracketLimit (n:int,m:int,maxBracketExpansions:int) (x0:double1) (dd0:double0) (f:double0->double1->unit) (df:double1->double1->unit) (fH:double2->double1->unit) (stepProc:((int0*double1)->unit)option) =
+        this.findminNewtonCore (n,m,maxBracketExpansions) x0 dd0 f df fH stepProc
 
     /// <summary>
     /// 準ニュートン法
@@ -268,7 +327,7 @@ type ContextOptimization internal (context:Aqualis) =
     /// <param name="dd0"></param>
     /// <param name="f"></param>
     /// <param name="df"></param>
-    member this.findmin_quasiNewton (n:int,m:int) (x0:double1) (dd0:double0) (f:double0->double1->unit) (df:double1->double1->unit) (stepProc:((int0*double1)->unit)option) =
+    member private this.findminQuasiNewtonCore (n:int,m:int,maxBracketExpansions:int) (x0:double1) (dd0:double0) (f:double0->double1->unit) (df:double1->double1->unit) (stepProc:((int0*double1)->unit)option) =
         match stepProc with
           |Some pu ->
                 pu (_0,x0)
@@ -304,7 +363,7 @@ type ContextOptimization internal (context:Aqualis) =
                         context.la.norm a <| fun nr -> context.br.if1 (nr.=0.0) <| fun () -> ext()
                         a.foreach <| fun i -> a.[i] <== -a.[i]
                         context.ch.d1 x0.size1 <| fun xx ->
-                            this.findmin m (x0,a) dd0_ f xx
+                            this.findminWithBracketLimit (m,maxBracketExpansions) (x0,a) dd0_ f xx
                             s.foreach <| fun j -> s.[j] <== xx.[j] - x0.[j]
                             df1 <== df0
                             x0.foreach <| fun i -> x0.[i] <== xx.[i]
@@ -313,6 +372,14 @@ type ContextOptimization internal (context:Aqualis) =
                                     pu(i,x0)
                               |None ->
                                 ()
+
+    /// 既定の探索区間拡張上限を用いて、準ニュートン法で極小値を探索します。
+    member this.findmin_quasiNewton (n:int,m:int) (x0:double1) (dd0:double0) (f:double0->double1->unit) (df:double1->double1->unit) (stepProc:((int0*double1)->unit)option) =
+        this.findminQuasiNewtonCore (n,m,OptimizationDefaults.MaxBracketExpansions) x0 dd0 f df stepProc
+
+    /// 探索区間の拡張回数に上限を指定して、準ニュートン法で極小値を探索します。
+    member this.findmin_quasiNewtonWithBracketLimit (n:int,m:int,maxBracketExpansions:int) (x0:double1) (dd0:double0) (f:double0->double1->unit) (df:double1->double1->unit) (stepProc:((int0*double1)->unit)option) =
+        this.findminQuasiNewtonCore (n,m,maxBracketExpansions) x0 dd0 f df stepProc
 
 [<AutoOpen>]
 module CompilationEnvironmentOptimizationExtensions =
---
+++ tests/Aqualis.Tests/OptimizationTests.fs
@@ -0,0 +1,65 @@
+namespace Aqualis.Tests
+
+open System
+open System.IO
+open System.Text.RegularExpressions
+open Xunit
+open Aqualis
+
+module OptimizationTests =
+    let private generateFindmin maxBracketExpansions =
+        use output = new TemporaryDirectory()
+        let project = "findmin-bracket-limit"
+
+        Compile [C99] output.Path project "1.0" <| fun context ->
+            let position = context.var.d1("position", 1)
+            let direction = context.var.d1("direction", 1)
+            context.optimization.findminWithBracketLimit
+                (3, maxBracketExpansions)
+                (position, direction)
+                (D 1.0)
+                (fun value point -> value <== -point.[0])
+                position
+
+        File.ReadAllText(Path.Combine(output.Path, project + ".c"))
+
+    [<Fact>]
+    let ``findmin rejects a negative bracket expansion limit`` () =
+        use context = Aqualis.BlankWriter C99
+        let position = context.var.d1("position", 1)
+        let direction = context.var.d1("direction", 1)
+
+        let error =
+            Assert.Throws<ArgumentException>(fun () ->
+                context.optimization.findminWithBracketLimit
+                    (3, -1)
+                    (position, direction)
+                    (D 1.0)
+                    (fun _ _ -> ())
+                    position)
+
+        Assert.Equal("maxBracketExpansions", error.ParamName)
+
+    [<Fact>]
+    let ``findmin emits a bounded bracket expansion and failure fallback`` () =
+        let generated = generateFindmin 2
+
+        Assert.Matches(Regex(@"if\s*\([^\r\n]*<\s*2\)"), generated)
+        Assert.Contains("&&", generated)
+        Assert.Contains("Aqualis: line-search bracket expansion limit reached.", generated)
+
+    [<Fact>]
+    let ``bounded variants are available for every higher-level optimizer`` () =
+        let methodNames =
+            typeof<ContextOptimization>.GetMethods()
+            |> Array.map _.Name
+            |> Set.ofArray
+
+        [
+            "findmin_GradientDescentWithBracketLimit"
+            "findmin_ConjugateGradient1WithBracketLimit"
+            "findmin_ConjugateGradient2WithBracketLimit"
+            "findmin_NewtonWithBracketLimit"
+            "findmin_quasiNewtonWithBracketLimit"
+        ]
+        |> List.iter (fun name -> Assert.Contains(name, methodNames))
